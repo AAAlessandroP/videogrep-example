7eff615e1d1f98aac88cc8ac455ab2e2eb0613f9
ale <ale@localhost.localdomain>
2020-06-01 23:32:39 +0200

    asd

--- app.js
+++ app.js
@@ -57,7 +57,7 @@ app.get("/", (req, res) => {
     
     <body>
     <ul>`;
-    fs.readdirSync("./public").forEach(filename => {
+    fs.readdirSync("public/").forEach(filename => {
         s += `<li><a href='/${filename}'>${filename}</a></li>`;
         // console.log(`filename`, filename);
     });
